53dff576a3b3f59ec1cf71e95a7957d26f3e1f86
John Howell <JonathanDavidHowell@gmail.com>
2019-11-20 20:38:00 -0500

    Initial python refactor

---
+++ simple_budget.py
@@ -0,0 +1,25 @@
+import math
+
+print("Welcome to John's Simple Budget. Please note that in rent I have bundled "
+      "water, trash, and sewage as that is how my personal situation is set up.\n")
+
+income = int(input("Enter your total monthly income: "))
+
+rent = int(input("Enter your monthly rent: "))
+electric = int(input("Enter your monthly electricity bill: "))
+internet = int(input("Enter your monthly internet bill: "))
+debt = int(input("Enter your monthly debt repayment: "))
+groc = int(input("Enter your weekly grocery bill: "))
+charity = int(input("Enter your monthly charity donations: "))
+
+expenses = [rent, electric, internet, debt, charity]
+
+leftover = income - (rent + electric + internet + debt + groc + charity)
+leftday = leftover / 30
+
+leftovers = [leftover, leftday]
+
+affrent = income * .3
+
+for expense in expenses:
+    print(expense.name + ": " + expense)
