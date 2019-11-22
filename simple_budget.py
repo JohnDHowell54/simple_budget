73d926e58a69a4507a02706d20c54eccf4f650a5
John <johndhowell54@live.com>
2019-11-21 20:15:39 -0500

    Got json exporting working in a test file
    Still need to check for path and export there

--- simple_budget.py
+++ simple_budget.py
@@ -1,30 +1,35 @@
 import math
+import csv
+import sys
+import json
 
-print("Welcome to John's Simple Budget. Please note that in rent I have bundled "
-      "water, trash, and sewage as that is how my personal situation is set up.\n")
+def calcBudget():
+    print("Welcome to John's Simple Budget. Please note that in rent I have bundled "
+          "water, trash, and sewage as that is how my personal situation is set up.\n")
 
-income = int(input("Enter your total monthly income: "))
+    income = int(input("Enter your total monthly income: "))
 
-rent = int(input("Enter your monthly rent: "))
-electric = int(input("Enter your monthly electricity bill: "))
-internet = int(input("Enter your monthly internet bill: "))
-debt = int(input("Enter your monthly debt repayment: "))
-groc = int(input("Enter your weekly grocery bill: "))
-charity = int(input("Enter your monthly charity donations: "))
-save = int(input("Enter your monthly savings: "))
-grocmonth = groc * 4
-expenses = {'rent': rent, 'electric': electric, 'internet': internet, 'debt': debt, 'charity': charity,
-            'groceries': grocmonth, 'savings':save}
-#
-# TODO: Daily/weekly allowance
-# Leftover money, to use as desired. Will factor into a daily allowance later
-#
-leftover = income - (rent + electric + internet + debt + groc + charity)
-leftday = leftover / 30
+    rent = int(input("Enter your monthly rent: "))
+    electric = int(input("Enter your monthly electricity bill: "))
+    internet = int(input("Enter your monthly internet bill: "))
+    debt = int(input("Enter your monthly debt repayment: "))
+    groc = int(input("Enter your weekly grocery bill: "))
+    charity = int(input("Enter your monthly charity donations: "))
+    save = int(input("Enter your monthly savings: "))
+    grocmonth = groc * 4
+    expenses = {'rent': rent, 'electric': electric, 'internet': internet, 'debt': debt, 'charity': charity,
+                'groceries': grocmonth, 'savings':save}
+    #
+    # TODO: Daily/weekly allowance
+    # Leftover money, to use as desired. Will factor into a daily allowance later
+    #
+    leftover = income - (rent + electric + internet + debt + groc + charity)
+    leftday = leftover / 30
 
-leftovers = [leftover, leftday]
+    leftovers = [leftover, leftday]
 
-affrent = income * .3
+    affrent = income * .3
+
 
 # Print our expenses
 for key, val in expenses.items():
@@ -36,7 +41,10 @@ if affrent > rent:
 else:
     print("Congratulations, you can afford your rent!")
 
+
 # TODO have budgets be persistent
+with open('budget.json', 'w') as fp:
+    json.dump(expenses, fp, indent=4, sort_keys=True)
 
 # TODO have options to load a budget, save a budget
 
